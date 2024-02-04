a44a4915f2169c74dba448fa29efbb2807d536a8
Jesus Hernandez <jesushdzusa@gmail.com>
2024-02-04 11:53:34 -0600

    responsive menu y ajustes mobile

--- website/static/script.js
+++ website/static/script.js
@@ -1,11 +1,12 @@
 
 /*DETERMINA QUE CAMPOS APARECEN Y QUE VALORES SE ENVIARAN PARA CALCULO CON BASE EN EL PERIODO QUE SE ELIJA*/
+/*
 let choicePeriodo = document.getElementById('periodo');
 let submit = document.getElementById('submit');
 document.addEventListener('click', (e) => {
     if (e.target.id === 'periodo') {
         let provisionales = document.getElementById('provisionales');
-        if (e.target.value === '1') {            
+        if (e.target.value === '1') {
             provisionales.disabled = true;
         } else {
             provisionales.disabled = false;
@@ -20,9 +21,22 @@ addEventListener('load', () => {
     let isr = document.getElementById('isr-cargo');
     let provisionales = document.getElementById('provisionales');
     choicePeriodo.value = '1';
-    inputIngreso.value=0;
-    inputRetencion.value=0;
-    isr.value=0;
+    inputIngreso.value = 0;
+    inputRetencion.value = 0;
+    isr.value = 0;
     provisionales.value = 0;
 })
 
+*/
+
+burger.addEventListener('click', () => {
+    let burger = document.getElementById('burger');
+    let fig = burger.innerHTML;
+    if (fig === '☰') {
+        burger.innerHTML = 'x';
+        
+    }
+    else {
+        burger.innerHTML = '☰';
+    }
+});
